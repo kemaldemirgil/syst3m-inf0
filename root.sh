e93bd858204bada3ed1e164c232f2c6267293b3c
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-06-19 01:45:41 -0400

    init

---
+++ root.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is a simple info command
+
+SOMEVAR2= tasklist  
+echo "$SOMEVAR2"
+echo "r00t"  
+ipconfig 
+SOMEVAR= systeminfo  
+echo "$SOMEVAR"
+SOMEVAR3= ipconfig 
+echo "$SOMEVAR3"
+echo "................................................." 
+echo "                  gam3"  
+echo "................................................."
+
+# gam3
