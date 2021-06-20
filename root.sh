edd68196e7deea37922bc55b1c8a44aa923f4be3
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-06-20 00:13:00 -0400

    style

--- root.sh
+++ root.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 # This is a simple info command
-
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo -e "\e[1;35m                 r00t_inf0        \n \e[0m"
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
 SOMEVAR2= tasklist  
 echo "$SOMEVAR2"
-echo "r00t"  
-ipconfig 
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo "r00t"
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+ipconfig
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
 SOMEVAR= systeminfo  
 echo "$SOMEVAR"
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
 SOMEVAR3= ipconfig 
 echo "$SOMEVAR3"
-echo "................................................." 
-echo "                  gam3"  
-echo "................................................."
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo -e "Architecture:\t\t"`arch`
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo -e "Hostname:\t\t"`hostname`
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
+echo -e "\e[1;35m    r00t_inf0 created by \e[1;30mgam3fac3\e[1;35m        \n \e[0m"
+echo -e "\e[1;30m<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>> \e[0m"
 
 # gam3
